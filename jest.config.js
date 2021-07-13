7e9cf1b1766b780161ffbdad34a967746a7d613d
Vo Anh Kiet <zvoanhkietz@gmail.com>
2021-07-13 22:10:58 +0700

    feat(app): init source for unit testing

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  testEnvironment: 'node',
+  verbose: true,
+  clearMocks: true,
+  roots: ['<rootDir>/src'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$',
+  moduleFileExtensions: ['js', 'jsx'],
+  collectCoverageFrom: [
+    'src/**/*.{js}',
+    '!src/configs/**',
+    '!**/node_modules/**',
+    '!**/build/**',
+    '!**/coverage/**'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  coverageReporters: ['text', 'text-summary']
+}
---
+++ src/__tests__/libs/validator.test.js
@@ -0,0 +1,39 @@
+import {describe, expect, test} from '@jest/globals'
+
+import Validator from '../../libs/validator'
+
+describe('Validator has', () => {
+  const validator = new Validator([
+    {
+      field: 'limit',
+      args: [[10, 20, 50, 100, 1000]],
+      check: 'inList',
+      message: 'limit must be in list [10, 20, 50, 100, 1000]'
+
+    }
+  ])
+  validator.addProvider({
+    inList (value, list) {
+      return list.includes(value)
+    }
+  })
+
+  test('isValid method return true is exactly', () => {
+    const resultTrue = validator.isValid({ limit: 10 })
+    expect(resultTrue).toBe(true)
+  })
+  test('isValid method return false is exactly', () => {
+    const resultTrue = validator.isValid({ limit: 1 })
+    expect(resultTrue).toBe(false)
+  })
+  test('getMessages method return error list when invalid', () => {
+    validator.isValid({ limit: 1 })
+    const messages = validator.getMessages()
+    expect(messages.length).toBe(1)
+    expect(messages[0]).toMatchObject({
+      value: 1,
+      field: 'limit',
+      message: 'limit must be in list [10, 20, 50, 100, 1000]'
+    })
+  })
+})
